8dcaa61c60f4b9504a1543ce4c6dcf43524184a8
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2021-12-31 00:01:01 +0200

    modify get all blogs

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authorizeBlogMW = require("../middlewares/blogMW");
 const { find, create, deleteDoc, update } = require("../controllers/blog");
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   find()
     .then((doc) => res.json(doc))
     .catch((e) => next(e));
